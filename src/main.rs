09a1a9d6ca1c00212818930a431838fe1be90f82
bryal <96.bryal@gmail.com>
2015-02-03 22:47:40 +0100

    It runs, but bad performance
    ca 15 fps

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate libc;
 
 use std::ptr;
+use std::mem;
 use std::ops::Drop;
 
 use libc::{uint32_t, uint8_t, c_void, size_t};
 
-type Byte = u8;
+// B8G8R8A8 pixel size in bytes
+static PIXEL_SIZE: u8 = 4;
 
 #[link(name = "DXGCap")]
 extern {
@@ -24,7 +26,7 @@ extern {
 
 struct Capturer {
 	dxgi_manager: *mut c_void,
-	current_frame: Vec<Byte>,
+	current_frame: Vec<u8>,
 }
 
 impl Capturer {
@@ -45,9 +47,9 @@ impl Capturer {
 		(width as usize, height as usize)
 	}
 
-	fn get_frame(&mut self) -> Result<&mut [Byte], ()> {
+	fn get_frame(&mut self) -> Result<&mut [u8], ()> {
 		let mut buffer_size: size_t = 0;
-		let mut buffer = ptr::null_mut::<Byte>();
+		let mut buffer = ptr::null_mut::<u8>();
 		if unsafe { get_frame_bytes(self.dxgi_manager, &mut buffer_size, &mut buffer) } {
 			if buffer_size == 0 {
 				Ok(self.current_frame.as_mut_slice())
@@ -67,29 +69,44 @@ impl Capturer {
 	}
 }
 
+impl Drop for Capturer {
+	fn drop(&mut self) {
+		unsafe {
+			let v = mem::replace(&mut self.current_frame, Vec::new());
+			mem::forget(v);
+			delete_dxgi_manager(self.dxgi_manager);
+		}
+	}
+}
+
+fn average_color(pixels_bytes: &[u8]) -> (u64, u64, u64) {
+	let n_pixels = pixels_bytes.len() as u64 / PIXEL_SIZE as u64;
+	let (mut r_sum, mut b_sum, mut g_sum) = (0u64, 0u64, 0u64);
+	for n in (0..n_pixels) {
+		let i = n as usize * PIXEL_SIZE as usize;
+		let (b, g, r) = (pixels_bytes[i], pixels_bytes[i+1], pixels_bytes[i+2]);
+		r_sum += r as u64;
+		b_sum += b as u64;
+		g_sum += g as u64;
+	}
+	(r_sum/n_pixels, b_sum/n_pixels, g_sum/n_pixels)
+}
+
 fn init_dxgi() {
 	unsafe { init(); }
 }
 
-impl Drop for Capturer {
-	fn drop(&mut self) {
-		unsafe { delete_dxgi_manager(self.dxgi_manager); }
-	}
-}
-
 fn main() {
 	init_dxgi();
-	{
-		let mut capturer = Capturer::new().unwrap();
+	let mut capturer = Capturer::new().unwrap();
 
-		let (width, height) = capturer.dimensions();
-		println!("{} x {}", width, height);
+	let (width, height) = capturer.dimensions();
+	println!("{} x {}", width, height);
 
-		for i in 0..60u8 {
-			println!("null frame: {}", capturer.get_frame().unwrap().iter().all(|b| *b == 0));
-		}
-		println!("Done1");
+	for i in 0..240u16 {
+		let frame = capturer.get_frame().unwrap();
+		// println!("null frame: {}", frame.iter().all(|b| *b == 0));
+		let avs = average_color(frame);
+		println!("Avs: {} {} {}", avs.0, avs.1, avs.2);
 	}
-	println!("Done2");
-
 }
